58db7001f2be295e07795c858edda8d8b3d183f3
Nikolas <86022356+nikk7007@users.noreply.github.com>
2021-08-07 19:12:47 -0300

    Update Bhaskara.py

--- mini-program/Bhaskara.py
+++ mini-program/Bhaskara.py
@@ -1,41 +1,62 @@
-from os import system
+# pip install colorama
+from os import system as sy
 from colorama import Fore
 
-verde = Fore.GREEN
-azul  = Fore.CYAN
-branco = Fore.WHITE
-reset  = Fore.RED
+cinza    = Fore.LIGHTBLACK_EX
+azul     = Fore.CYAN
+roxo     = Fore.LIGHTMAGENTA_EX
+verde    = Fore.LIGHTGREEN_EX
+alert    = Fore.RED
+reset    = Fore.RESET
 
-def bhaskara():
+def line(cor):
+    print(cor + '#'*30 + reset)
 
-    system('cls')
-    print('{}{}{}'.format(azul, '#'*30, reset))
 
-    a = float(input('a= {}'.format(azul))) 
-    b = float(input('{}b= {}'.format(reset, azul))) 
-    c = float(input('{}c= {}'.format(reset, azul))) 
+while True:
+    sy('cls')
+    line(azul)
 
-    print('{}{}{}'.format(azul, '#'*30, branco))
+    # Validação para a, b, c com certeza serem números
+    while True:
+        a = input(f'{verde}a = {reset}').strip()
+        b = input(f'{verde}b = {reset}').strip()
+        c = input(f'{verde}c = {reset}').strip()
 
-    delta  = (b**2 - 4*a*c)
-    rdelta = delta**(1/2)
-    
-    if delta < 0:
-        print('Resultado VAZIO, delta é negativo')
+        if a.isnumeric() == True and b.isnumeric() and c.isnumeric():
+            a = float(a)
+            b = float(b)
+            c = float(c)
+            break
+
+        sy('cls')
+        line(azul)
+        print(f'{alert}ERROR. Por favor, digite apenas NUMEROS')
+
+    line(azul)
+
+    # Calculando delta e raiz de delta
+    d  = b**2 - 4*a*c
+    rd = d**.5
+
+    print(roxo, end='')
+    if d < 0:
+        print(f'Delta negativo, resultado VAIZIO')
+
     else:
-        print('valor de delta:', delta)
-        print('valor da raiz de delta:', rdelta)
+        # Calculando x1 e x2
+        x1 = (0-b + rd) / (2 * a) 
+        x2 = (0-b - rd) / (2 * a) 
 
-        x1 = (0-b + rdelta) / (2 * a)
-        x2 = (0-b - rdelta) / (2 * a)
+        print(f'Valor de delta: {d}')
+        print(f'Valor da raiz de delta: {rd}')
+        print(f'x1: {x1} ||| x2: {x2}')
         
-        print(branco + 'X1: ', x1, end=' ||| ')
-        print('X2 {} \n'.format(x2))
-
-    reuse = input('{}Deseja reutilizar o programa? (s/n) {}'.format(azul, reset))
-    system('cls')
-
-    if reuse == 's':
-        bhaskara()
-
-bhaskara()
+    while True:
+        reuse = input(f'Quer usar o programa novamente? [s/n]:{reset} ').strip().lower()[0]
+        if reuse in 'sn':
+            break
+    
+    if reuse == 'n':
+        sy('cls')
+        break
